ab4aad6b4dd2a1636e086bf170e5a8895b74334b
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2025-12-13 14:14:08 +0100

    service

---
+++ src/main/java/com/server/activity/services/UserServiceClient.java
@@ -0,0 +1,10 @@
+package com.server.activity.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceClient {
+
+}
