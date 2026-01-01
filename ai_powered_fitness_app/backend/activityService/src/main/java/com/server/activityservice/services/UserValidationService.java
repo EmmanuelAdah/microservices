dffd2763b3b0c881b2a96c4d8af10fb0e27a39b6
Emmanuel Adah Edogbanya <edogbanya02@gmail.com>
2026-01-01 23:58:06 +0100

    feat: user validation

--- ai_powered_fitness_app/backend/activityService/src/main/java/com/server/activityservice/services/UserValidationService.java
+++ ai_powered_fitness_app/backend/activityService/src/main/java/com/server/activityservice/services/UserValidationService.java
@@ -1,12 +1,9 @@
 package com.server.activityservice.services;
 
 import com.server.activityservice.exceptions.InvalidUserRequestException;
-import com.server.activityservice.exceptions.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +11,6 @@ public class UserValidationService {
     private final WebClient userValidationWebClient;
 
     public void isValidUser(String userId) {
-        try {
             Boolean isValidUser = userValidationWebClient
                     .get()
                     .uri("/api/user/validate/{userId}", userId)
@@ -25,13 +21,5 @@ public class UserValidationService {
             if (Boolean.FALSE.equals(isValidUser)) {
                 throw new InvalidUserRequestException("Invalid request with id: " + userId);
             }
-
-        } catch (WebClientResponseException ex) {
-            if (ex.getStatusCode() == HttpStatus.NOT_FOUND) {
-                throw new UserNotFoundException("User not found with id: " + userId);
-            }else if (ex.getStatusCode() == HttpStatus.BAD_REQUEST) {
-                throw new InvalidUserRequestException("Invalid request with id: " + userId);
-            }
-        }
     }
 }
